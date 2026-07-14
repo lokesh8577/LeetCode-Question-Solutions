12c5e108679fe17ce17561849dd11e53d2cc5d0f
Lokesh Prajapat <165503393+lokesh8577@users.noreply.github.com>
2026-07-14 16:29:12 +0530

    Time: 0 ms (100%), Space: 10 MB (17.96%) - LeetHub

---
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        unordered_map<char, int> mp;
+        for (int i = 0; i < t.size(); i++) {
+            mp[t[i]] = i;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != t[i]) {
+                ans += abs(i - mp[s[i]]);
+            }
+        }
+        return ans;
+    }
+};
