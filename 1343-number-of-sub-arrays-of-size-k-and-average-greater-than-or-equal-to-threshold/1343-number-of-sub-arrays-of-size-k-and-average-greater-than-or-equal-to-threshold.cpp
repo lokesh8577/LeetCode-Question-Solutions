88de44a483e7a7f8b8308050774c2c256b48cf34
Lokesh Prajapat <165503393+lokesh8577@users.noreply.github.com>
2026-07-18 03:14:20 +0530

    Time: 0 ms (100%), Space: 59 MB (97.61%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
         long long windowSum = 0;
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < k; i++) { // taking first window and store total sum
+                                      // of that window in windowSum
             windowSum += arr[i];
         }
         int total = 0;
-        if (windowSum / k >= threshold) {
+        if (windowSum / k >=
+            threshold) { // check the condition is averagge of window is greater
+                         // or equal to threshold or not
             total++;
         }
 
-        for (int i = k; i < arr.size(); i++) {
+        for (int i = k; i < arr.size(); i++) { // now checking all the window of
+                                               // size k and check the condition
             windowSum += arr[i];
             windowSum -= arr[i - k];
             if (windowSum / k >= threshold) {
-            total++;
+                total++;
+            }
         }
-        }
-        return total;
+        return total;//return total sub array which statisfy the condition
     }
 };
